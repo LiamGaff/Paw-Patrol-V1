d8204cb05a9a86577029c97db1f232818eefae4e
LiamGaff <liamgaffneytya@gmail.com>
2021-09-22 11:47:10 +0000

    Registered Animal and Volunteer models/ customized admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Volunteer, Animal
 
-# Register your models here.
+
+class VolunteerAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "friendly_name",
+        "about",
+        "image",
+    )
+
+
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'type',
+        'breed',
+        'about',
+        "image",
+    )
+
+
+admin.site.register(Volunteer, VolunteerAdmin)
+admin.site.register(Animal, AnimalAdmin)
