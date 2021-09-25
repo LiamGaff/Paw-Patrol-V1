e6b28806f45c3f8c1d933a48e91b585faa933b7c
LiamGaff <liamgaffneytya@gmail.com>
2021-09-25 12:01:29 +0000

    added stripe elemnt to donation.html

--- donation/urls.py
+++ donation/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.donate, name='donations')
+    path('', views.donate, name='donate'),
+    path('charge/', views.charge, name='charge'),
+    path('success/<str:args>/', views.successMsg, name='success'),
 ]
--- paw_patrol/urls.py
+++ paw_patrol/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('donations/', include('donation.urls')),
+    path('donate/', include('donation.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
